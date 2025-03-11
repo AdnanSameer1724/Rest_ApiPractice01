cc7029dcbf73b57b1de784978d59936b7800ca1d
adnan50 <adnan@livekeeping.com>
2025-03-11 22:05:54 +0530

    Api building for signUp & login

--- models/authentication.js
+++ models/authentication.js
@@ -1,14 +1,36 @@
+require('dotenv').config();
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+
 
 const dataSchema = new mongoose.Schema({
     emailID: {
         type: String,
         required: true,
+        unique: true,
     },
     password: {
         type: String,
         required: true,
     },
-})
+    token: {
+        type: String,
+    },
+});
+
+dataSchema.pre('save',async function(next) {
+    if(!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+dataSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({_id: this._id, emailID: this.emailID}, process.env.JWT_SECRET, { expiresIn: '1h'});
+    this.token = token;
+    return token;
+};
 
 module.exports = mongoose.model('Authentication', dataSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,62 @@
 const express = require('express');
 const router = express.Router();
 const Authentication = require('../models/authentication');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-router.get('/login', getDetails,async (req, res) => {
-    res.send(res.user.name)
-    // try{
-    //     const dataBase = await Authentication.find();
-    //     res.send(dataBase);
-    // } catch(err){
-    //     res.status(500).json({ message: err.message })
-    // }
-})
+router.get('/', (req, res) => {
+    res.json({ message: "Welcome to API"});
+});
 
-router.post('/signup',async (req, res) => {
-    const dataBase = new Authentication({
-        emailID: req.body.emailID,
-        password: req.body.password,
-    })
+router.post ('/login', async (req, res) => {
     try{
-        const newUser = await dataBase.save()
-        res.status(201).json(newUser)
-    }catch(err){
-        res.status(400).json({ message: err.message })
-    }
-})
+        const { emailID, password } = req.body;
 
-
-async function getDetails(req, res, next){
-    let user
-    try{
-        user = await Authentication.findById(req.params.id)
-        if(user == null){
-            return res.status(404).json({ message: "1st message"})
+        const user = await Authentication.findOne({ emailID }); 
+        if(!user){
+            return res.status(400).json({ message: 'Invalid Username' });
         }
-    }catch(err){
-        return res.status(500).json({ message: err.message })
-    }
+        
 
-    res.user = user
-    next()
-}
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({ message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ emailID: user.emailID }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+
+        res.status(200).json({ message: 'Login successfull', token})
+    }
+    catch(err){
+        res.status(500).json({ message: err.message });
+    }
+});
+  
+router.post('/signup', async (req, res) => {
+    try{
+        const { emailID, password } = req.body;
+
+        const existingUser = await Authentication.findOne({ emailID });
+        if(existingUser){
+            return res.status(400).json({ message: 'Account already exists'});
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new Authentication({
+            emailID,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({ message: 'User created successfully' });
+    }catch(err){
+        res.status(500).json({ message: 'Error registering user', error: err.message });
+    }
+});
+
 module.exports = router;

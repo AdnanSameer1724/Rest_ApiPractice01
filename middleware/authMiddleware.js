2f27b426fd171825706046179d4a904e26b0a21b
adnan50 <adnan@livekeeping.com>
2025-03-12 22:43:39 +0530

    Authentication completed & initialized profile route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,25 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization');
-    if(!token) return res.status(401).json({ message: 'Access Denied. No Token Provided' });
+  const token = req.header("token");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied. No Token Provided" });
+  }
 
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    }catch(err){
-        return res.status(400).json({ message: 'Invalid Token' });
-    }
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+    console.log("decoded:",decoded);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Invalid or Expired Token" });
+  }
 };
 
 module.exports = authMiddleware;
--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,91 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Authentication = require('../models/authentication');
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const Authentication = require("../models/authentication");
+const authMiddleware = require("../middleware/authMiddleware");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-router.get('/', (req, res) => {
-    res.json({ message: "Welcome to API"});
+router.get("/", (req, res) => {
+  res.json({ message: "Welcome to API" });
 });
 
-router.post('/forgetPassword', async (req, res) => {
-    try{
-        const { emailID, password, confirmPassword } = req.body;
+router.post("/forgetPassword", async (req, res) => {
+  try {
+    const { emailID, password, confirmPassword } = req.body;
 
-        const user = await Authentication.findOne({ emailID });
-        if(!user){
-            return res.status(401).json({ message:"User doesn't exist" })
-        }
-
-        if(password !== confirmPassword){
-            return res.status(401).json({ message: "Password doesn't match"})
-        }
-
-        user.password = password;
-        await user.save();
-        return res.status(200).json({ message: "Password updated successfully" });
-
-    }catch(error){
-        console.error("Error updating password:", error);
-        return res.status(500).json({ message: "Internal Server Error" });
+    const user = await Authentication.findOne({ emailID });
+    if (!user) {
+      return res.status(401).json({ message: "User doesn't exist" });
     }
+
+    if (password !== confirmPassword) {
+      return res.status(401).json({ message: "Password doesn't match" });
+    }
+
+    user.password = password;
+    await user.save();
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error("Error updating password:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.post ('/login', async (req, res) => {
-    try{
-        const { emailID, password } = req.body;
+router.post("/login", async (req, res) => {
+  try {
+    const { emailID, password } = req.body;
 
-        const user = await Authentication.findOne({ emailID }); 
-        if(!user){
-            return res.status(400).json({ message: 'Invalid Username' });
-        }
-        
-        const isMatch = bcrypt.compareSync(password, user.password);
-        if(!isMatch){
-            return res.status(400).json({ message: 'Invalid password' });
-        }
-
-        const token = jwt.sign({ emailID: user.emailID }, process.env.JWT_SECRET, {
-            expiresIn: '1h',
-        });
-
-        res.status(200).json({ message: 'Login successfull', token})
+    const user = await Authentication.findOne({ emailID });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Username" });
     }
-    catch(err){
-        res.status(500).json({ message: err.message });
+
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
     }
+
+    const token = jwt.sign({ emailID: user.emailID }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "Login successfull", token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
-  
-router.post('/signup', async (req, res) => {
-    try{
-        const { emailID, password } = req.body;
 
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(emailID)){
-            return res.status(400).json({ message: "Invalid email format" });
-        }
+router.post("/signup", async (req, res) => {
+  try {
+    const { emailID, password } = req.body;
 
-        const existingUser = await Authentication.findOne({ emailID });
-        if(existingUser){
-            return res.status(400).json({ message: 'Account already exists'});
-        }
-
-        const newUser = new Authentication({
-            emailID,
-            password
-        });
-
-        await newUser.save();
-
-        res.status(201).json({ message: 'User created successfully' });
-    }catch(err){
-        res.status(500).json({ message: 'Error registering user', error: err.message });
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailID)) {
+      return res.status(400).json({ message: "Invalid email format" });
     }
+
+    const existingUser = await Authentication.findOne({ emailID });
+    if (existingUser) {
+      return res.status(400).json({ message: "Account already exists" });
+    }
+
+    const newUser = new Authentication({
+      emailID,
+      password,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+});
+
+router.get("/dashboard", authMiddleware, (req, res) => {
+    res.status(200).json({ message: "Welcome Onboard", user: req.user });
 });
 
 module.exports = router;
